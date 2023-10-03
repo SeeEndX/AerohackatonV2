72b5a82b88b8dc71405c98ded819b719b5d07cee
SeeEndX <ceh938of@yandex.ru>
2023-10-03 15:21:58 +0300

    начало разметки мест на схеме самолета

--- app/src/main/java/com/example/aerohackatonnew/screens/FlightScreen.kt
+++ app/src/main/java/com/example/aerohackatonnew/screens/FlightScreen.kt
@@ -1,19 +1,33 @@
 package com.example.aerohackatonnew.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -57,12 +71,9 @@ fun FlightScreen(flight: Flight?){
             shape = MaterialTheme.shapes.medium
         ) {
             // разметка схемы самолета
-            LazyVerticalGrid(
-                columns = ,
-                content =
-            ){
-
-            }
+            AircraftSeatLayout(
+                seats = listOf("A", "B", "C", "D", "E", "F"),
+            )
         }
         Spacer(modifier = Modifier.height(16.dp))
         Button(
@@ -75,5 +86,50 @@ fun FlightScreen(flight: Flight?){
         ) {
             Text(text = "Создать отчет")
         }
+    }
+}
+
+@Composable
+fun AircraftSeatLayout(seats: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        itemsIndexed(seats) { index, seat ->
+            if (index % 12 == 0) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(4.dp)
+                ) {
+                    CreateSeatBox(seat)
+                }
+            } else if (index % 6 == 0) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CreateSeatBox(seat)
+            } else {
+                CreateSeatBox(seat)
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateSeatBox(seat: String) {
+    Box(
+        modifier = Modifier
+            .size(50.dp)
+            .background(Color.Gray)
+            .clickable {
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = seat,
+            color = Color.White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
